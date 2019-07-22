f538ce6d6dffe8f2e41899255793663958bb9144
cChansik Yun <chansigi@gmail.com>
2019-07-22 05:15:12 +0000

    .

--- 0722/3_테스트픽스쳐.cpp
+++ 0722/3_테스트픽스쳐.cpp
@@ -1,3 +1,5 @@
+#include <gtest/gtest.h>
+
 class Calculator {
 public:
 	void Enter(double value) {}
@@ -6,6 +8,7 @@ public:
 	
 	double Display() { return 0; }
 };
+
 //------------------------------------
 // 3_테스트픽스쳐
 //
@@ -21,13 +24,40 @@ public:
 //  - 픽스쳐 설치와 검증 로직에 대한 부분이 테스트 함수 안에 동시에 존재하기 때문에, 테스트 인과 관계 분석이 용이하다.
 //  단점
 //  - 모든 테스트 함수 안에서 픽스쳐 설치 코드가 중복된다.
+
+// xUnit Test Pattern
+//  TestSuite
+//    TestCase1
+//    TestCase2
+//
+// Google Test
+//  TestCase
+//     Test1
+//     Test2
+
+
+// ::testing::Test
+//       |
+//    CalculatorTest
+//    |      |       |
+//  TEST_F TEST_F  TEST_F
+
 // 2) Delegation Setup
 //  - 픽스쳐 설치에 대한 중복 코드를 별도의 함수로 묶는다.
+// 1) 명시적인 테스트 케이스를 별도로 작성해야 합니다.
+class CalculatorTest : public ::testing::Test {
+// private:
+protected:
+    // TEST_F로 만든 자식 클래스의 테스트 함수 안에서 해당 기능에 접근하는 것이 가능합니다.
+	Calculator* Create() { return new Calculator; }
+	// Test Utility Method
+	//   : 구글 테스트에서는 protected로 만들어야 합니다.
+};
 
-
-TEST(CalculatorTest, Test1) {
+// 2) TEST_F 라는 매크로를 통해 테스트 함수를 작성해야 합니다.
+TEST_F(CalculatorTest, Test1) {
 	// Arrange
-	Calculator* calc = new Calculator;
+	Calculator* calc = Create();
 
 	// Act
 	calc->Enter(2);
@@ -37,9 +67,9 @@ TEST(CalculatorTest, Test1) {
 	ASSERT_EQ(4, calc->Display()) << "2 + 2 == 4";
 }
 
-TEST(CalculatorTest, Display_2Plus2_Equals4) {
+TEST_F(CalculatorTest, Display_2Plus2_Equals4) {
 	// Arrange
-	Calculator* calc = new Calculator;
+	Calculator* calc = Create();
 
 	// Act
 	calc->Enter(2);
