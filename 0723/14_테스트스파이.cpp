814222d98e4070158297db0ff4d30224ef24b7bd
cChansik Yun <chansigi@gmail.com>
2019-07-23 06:02:03 +0000

    .

--- 0723/14_테스트스파이.cpp
+++ 0723/14_테스트스파이.cpp
@@ -26,3 +26,55 @@ public:
 	}
 };
 
+//-----------------------------------------------
+#include <gtest/gtest.h>
+#include <algorithm> // find
+
+// SUT의 함수를 호출하였을 때, 발생하는 부수 효과를 관찰할 수 없어서, 테스트 안된 요구 사항이 있다.
+// => 테스트 스파이 패턴
+//   : 목격한 일을 기록해두었다가, 나중에 테스트에서 사용할 수 있도록 만들어진 테스트 대역
+
+class SpyTarget : public DLogTarget {
+	std::vector<std::string> logs;
+
+	std::string concat(DLogLevel level, const std::string& message) {
+		static const char* level_string[] = {
+			"INFO", "WARN", "ERROR",
+		};
+		return std::string(level_string[level]) + "@" + message;
+	}
+
+public:
+	void Write(DLogLevel level, const std::string& message) {
+		logs.push_back(concat(level, message));
+	}
+
+	bool IsReceived(DLogLevel level, const std::string& message) {
+		return std::find(logs.begin(), logs.end(), 
+				concat(level, message)) != logs.end();
+	}
+};
+
+
+class DLogTest : public ::testing::Test {
+};
+
+TEST_F(DLogTest, WriteTest) {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
