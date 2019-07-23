bed794f64d6044a5a8be732666032854bc2c802d
cChansik Yun <chansigi@gmail.com>
2019-07-23 04:24:32 +0000

    .

--- 0723/12_테스트스텁.cpp
+++ 0723/12_테스트스텁.cpp
@@ -39,14 +39,27 @@ public:
 //-----------------------------
 // 네트워크 연결이 존재하지 않을 때, User Move의 동작이 예외를 던지는지 확인하고 싶다.
 // => 특수한 상황에서 SUT의 동작을 확인하고 싶다.
+// => 제어할 수 없는 외부 의존객체의 동작을 스텁을 통해 변경할 수 있다.
+// => '파일 시스템', '네트워크', '시간'
 #include <gtest/gtest.h>
+
+class StubConnection : public IConnection {
+public:
+	virtual void Move(int x, int y) {
+		throw IOException();
+	}
+
+	virtual void Attack(User* p) {
+		throw IOException();
+	}
+};
 
 class UserTest : public ::testing::Test {
 };
 
 
 TEST_F(UserTest, MoveTest) {
-	TCPConnection conn;
+	StubConnection conn;
 	User user(&conn);
 
 	ASSERT_THROW(user.Move(10, 32), IOException);
